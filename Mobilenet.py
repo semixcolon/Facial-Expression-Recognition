ebdaa2726e5984253368bb59451e0551b54977cc
Lawrence Icaro <34086883+semixcolon@users.noreply.github.com>
2020-06-03 06:30:33 +0800

    Update and rename Classification_mobile_net.py to Mobilenet.py

--- Mobilenet.py
+++ Mobilenet.py
@@ -1,28 +1,24 @@
 from keras.applications import MobileNet
 from keras.models import Sequential,Model 
-from keras.layers import Dense,Dropout,Activation,Flatten,GlobalAveragePooling2D
+from keras.layers import 
+
+Dropout,Activation,Flatten,GlobalAveragePooling2D
 from keras.layers import Conv2D,MaxPooling2D,ZeroPadding2D
 from keras.layers.normalization import BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 
-# MobileNet is designed to work with images of dim 224,224
 img_rows,img_cols = 224,224
-
+num_classes = 8
 MobileNet = MobileNet(weights='imagenet',include_top=False,input_shape=(img_rows,img_cols,3))
 
-# Here we freeze the last 4 layers
-# Layers are set to trainable as True by default
 
 for layer in MobileNet.layers:
     layer.trainable = True
 
-# Let's print our layers
 for (i,layer) in enumerate(MobileNet.layers):
     print(str(i),layer.__class__.__name__,layer.trainable)
 
 def addTopModelMobileNet(bottom_model, num_classes):
-    """creates the top or head of the model that will be 
-    placed ontop of the bottom layers"""
 
     top_model = bottom_model.output
     top_model = GlobalAveragePooling2D()(top_model)
@@ -36,7 +32,7 @@ def addTopModelMobileNet(bottom_model, num_classes):
 
     return top_model
 
-num_classes = 5
+
 
 FC_Head = addTopModelMobileNet(MobileNet, num_classes)
 
@@ -44,8 +40,14 @@ model = Model(inputs = MobileNet.input, outputs = FC_Head)
 
 print(model.summary())
 
-train_data_dir = '/Users/durgeshthakur/Deep Learning Stuff/Emotion Classification/fer2013/train'
-validation_data_dir = '/Users/durgeshthakur/Deep Learning Stuff/Emotion Classification/fer2013/validation'
+
+train_data_dir =  'train path'
+validation_data_dir = 'test path'
+
+nb_train_samples = 
+nb_validation_samples = 
+epochs = 20
+batch_size = 10
 
 train_datagen = ImageDataGenerator(
                     rescale=1./255,
@@ -58,7 +60,6 @@ train_datagen = ImageDataGenerator(
 
 validation_datagen = ImageDataGenerator(rescale=1./255)
 
-batch_size = 32
 
 train_generator = train_datagen.flow_from_directory(
                         train_data_dir,
@@ -70,24 +71,19 @@ train_generator = train_datagen.flow_from_directory(
 validation_generator = validation_datagen.flow_from_directory(
                             validation_data_dir,
                             target_size=(img_rows,img_cols),
-                            batch_size=batch_size,
+                            batch_size=batch_size, shuffle = False,
                             class_mode='categorical')
 
 from keras.optimizers import RMSprop,Adam
 from keras.callbacks import ModelCheckpoint,EarlyStopping,ReduceLROnPlateau
 
 checkpoint = ModelCheckpoint(
-                             'emotion_face_mobilNet.h5',
+                             'mobilenet.h5',
                              monitor='val_loss',
                              mode='min',
                              save_best_only=True,
                              verbose=1)
 
-earlystop = EarlyStopping(
-                          monitor='val_loss',
-                          min_delta=0,
-                          patience=10,
-                          verbose=1,restore_best_weights=True)
 
 learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
                                             patience=5, 
@@ -95,17 +91,12 @@ learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
                                             factor=0.2, 
                                             min_lr=0.0001)
 
-callbacks = [earlystop,checkpoint,learning_rate_reduction]
+callbacks = [checkpoint,learning_rate_reduction]
 
 model.compile(loss='categorical_crossentropy',
               optimizer=Adam(lr=0.001),
               metrics=['accuracy']
               )
-
-nb_train_samples = 24176
-nb_validation_samples = 3006
-
-epochs = 25
 
 history = model.fit_generator(
             train_generator,
@@ -115,5 +106,38 @@ history = model.fit_generator(
             validation_data=validation_generator,
             validation_steps=nb_validation_samples//batch_size)
 
+import matplotlib.pyplot as plt
+fig, axes = plt.subplots(1,2, figsize=(18, 6))
+
+axes[0].plot(history.history['acc'])
+axes[0].plot(history.history['val_acc'])
+axes[0].set_title('Model accuracy')
+axes[0].set_ylabel('Accuracy')
+axes[0].set_xlabel('Epoch')
+axes[0].legend(['Train', 'Validation'], loc='upper left')
 
 
+axes[1].plot(history.history['loss'])
+axes[1].plot(history.history['val_loss'])
+axes[1].set_title('Model loss')
+axes[1].set_ylabel('Loss')
+axes[1].set_xlabel('Epoch')
+axes[1].legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+
+#evaluation
+import keras
+from keras.utils.generic_utils import CustomObjectScope
+from keras.models import load_model
+with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D}):
+    model = load_model('mobilenet.h5')
+from sklearn.metrics import classification_report, confusion_matrix
+
+Y_pred = model.predict_generator(validation_generator, nb_validation_samples // batch_size+1)
+y_pred = np.argmax(Y_pred, axis=1)
+print('Confusion Matrix')
+print(confusion_matrix(validation_generator.classes, y_pred))
+print('Classification Report')
+target_names = ['Angry','Disgust','Fear','Happy','Sad','Surprise','Neutral','Contempt']
+print(classification_report(validation_generator.classes, y_pred, target_names=target_names))
